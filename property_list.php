3aa3b455747170584d65f3bd3043f3ac1499e664
Kritesh-Shrivastava <kriteshshri@gmail.com>
2022-09-23 10:04:36 +0530

    Update property_list.php

--- property_list.php
+++ property_list.php
@@ -70,21 +70,6 @@ $interested_users_properties = mysqli_fetch_all($result_3, MYSQLI_ASSOC);
     </nav>
 
     <div class="page-container">
-        <div class="filter-bar row justify-content-around">
-            <div class="col-auto" data-toggle="modal" data-target="#filter-modal">
-                <img src="img/filter.png" alt="filter" />
-                <span>Filter</span>
-            </div>
-            <div class="col-auto">
-                <img src="img/desc.png" alt="sort-desc" />
-                <span>Highest rent first</span>
-            </div>
-            <div class="col-auto">
-                <img src="img/asc.png" alt="sort-asc" />
-                <span>Lowest rent first</span>
-            </div>
-        </div>
-
         <?php
         foreach ($properties as $property) {
             $property_images = glob("img/properties/" . $property['id'] . "/*");
